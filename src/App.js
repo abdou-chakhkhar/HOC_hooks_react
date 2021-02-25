4d794afccc5113608e1ac2b64bec52081df1a9cc
Abdou <abdelmoula.chakhkhar@uit.ac.ma>
2021-02-25 20:41:43 +0100

    save it to github

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Movie from './hoc/Movie';
+import Counter from './hooks/Counter';
+import Users from './hooks/Users';
+
+
+class App extends Component {
+
+
+
+  render() {
+    return (
+      // <Movie />
+      //<Counter />
+      <Users />
+    );
+  }
+}
+
+export default App;
